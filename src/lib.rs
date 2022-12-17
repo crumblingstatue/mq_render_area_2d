02c941c0d29952d17512021f32e3bc9be1f57ffe
crumblingstatue <radiantstatue@gmail.com>
2022-12-18 00:11:57 +0100

    Improve documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,49 @@
+//! Provides a 2d render area with a camera.
+//!
+//! The render area has a "virtual" size that is independent of the actual window size.
+//! Then it can be scaled to fit the actual window size.
+
+#![warn(missing_docs)]
+
 use macroquad::prelude::*;
 
 /// Acts as a regular screen with the specified dimensions when drawing to it,
 /// but has functionality to be easily rescaled and centered.
+///
+/// Example usage:
+///
+/// ```
+/// use macroquad::prelude::*;
+/// use mq_render_area_2d::RenderArea2D;
+///
+/// #[macroquad::main("renderarea2dtest")]
+/// async fn main() {
+///     let mut ra = RenderArea2D::new(400, 300);
+///     ra.center_camera(0., 0.);
+///     loop {
+///
+///         if is_key_down(KeyCode::Left) {
+///             ra.move_camera(-2., 0.);
+///         }
+///         if is_key_down(KeyCode::Up) {
+///             ra.move_camera(0., -2.);
+///         }
+///         if is_key_down(KeyCode::Right) {
+///             ra.move_camera(2., 0.);
+///         }
+///         if is_key_down(KeyCode::Down) {
+///             ra.move_camera(0., 2.);
+///         }
+///
+///         ra.set();
+///         clear_background(BLACK);
+///         draw_circle(0.0, 0.0, 100.0, YELLOW);
+///         set_default_camera();
+///         ra.draw();
+///         next_frame().await;
+///     }
+/// }
+/// ```
 pub struct RenderArea2D {
     render_target: RenderTarget,
     width: u32,
@@ -15,6 +57,7 @@ fn target(width: u32, height: u32) -> Vec2 {
 }
 
 impl RenderArea2D {
+    /// Create a new render area with the specified virtual resolution.
     pub fn new(width: u32, height: u32) -> Self {
         let rt = render_target(width, height);
         let cam = Camera2D {
@@ -34,12 +77,17 @@ impl RenderArea2D {
         s.set_scale_auto();
         s
     }
+    /// Sets this render area for drawing.
+    ///
+    /// Call this before drawing into the render area.
     pub fn set(&self) {
         set_camera(&self.camera);
     }
+    /// Set the scale to an integer amount. 2 is 2x zoom for example.
     pub fn set_scale(&mut self, amount: u8) {
         self.scale = amount;
     }
+    /// Set the scale automatically to fit the window size.
     pub fn set_scale_auto(&mut self) {
         self.scale = self.auto_scale();
     }
@@ -53,9 +101,13 @@ impl RenderArea2D {
             ver_ratio
         }) as u8
     }
+    /// Get the integer scale
     pub fn scale(&self) -> u8 {
         self.scale
     }
+    /// Draw this render area to the window.
+    ///
+    /// You need to first set the default camera with macroquad's `set_default_camera()`.
     pub fn draw(&self) {
         let params = DrawTextureParams {
             dest_size: Some(vec2(
@@ -88,9 +140,11 @@ impl RenderArea2D {
             (screen_height() - self.height as f32 * self.scale as f32) / 2.0,
         )
     }
+    /// Move the camera (x, y) by the specified amounts
     pub fn move_camera(&mut self, x: f32, y: f32) {
         self.camera.target += vec2(x, y);
     }
+    /// Center the camera on (x, y)
     pub fn center_camera(&mut self, x: f32, y: f32) {
         self.camera.target = vec2(x + 16.0, y + 16.0);
     }
